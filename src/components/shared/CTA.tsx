7a18b8f38db19db335d5694856ca8f5dc9469b26
saisaynoomleng <saileng9723@gmail.com>
2026-04-25 13:34:17 -0400

    finish signUpPage

--- src/components/shared/CTA.tsx
+++ src/components/shared/CTA.tsx
@@ -4,11 +4,24 @@ import Link from 'next/link';
 import { GoArrowRight } from 'react-icons/go';
 import clsx from 'clsx';
 
-const CTA = ({ className, href }: CTAProps) => {
+const CTA = ({ className, href, variant = 'text', children }: CTAProps) => {
   return (
-    <Button asChild variant="cta" className={clsx(className)}>
-      <Link href={href} aria-label="call to action">
-        <GoArrowRight className="-rotate-45 group-hover:rotate-0 transition-all duration-200 ease-in-out group-hover:text-brand-white-50" />
+    <Button asChild variant="cta">
+      <Link
+        href={href}
+        aria-label="call to action"
+        className={clsx(
+          variant === 'arrow'
+            ? 'bg-brand-black-100/20 rounded-full group hover:bg-brand-black-100 w-8 aspect-square'
+            : 'bg-transparent block! text-brand-black-200! rounded-none underline hover:text-brand-red-300!',
+          className,
+        )}
+      >
+        {variant === 'arrow' ? (
+          <GoArrowRight className="-rotate-45 group-hover:rotate-0 transition-all duration-200 ease-in-out group-hover:text-brand-white-50" />
+        ) : (
+          <span>{children}</span>
+        )}
       </Link>
     </Button>
   );
